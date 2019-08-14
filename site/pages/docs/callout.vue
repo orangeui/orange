2cb2da3fc4d2f65541d36800a27225fec617ae5e
Lucijan Kranjc <lucijan.kranjc@gmail.com>
2019-08-14 13:33:01 +0200

    add Callout examples to docs, cleanup

--- site/pages/docs/callout.vue
+++ site/pages/docs/callout.vue
@@ -14,8 +14,8 @@
     <!-- Basic -->
     <docs-item title="Basic" :code="callout_html" v-if="is_html">
       <div slot="body">
-        <div class="callout callout--color-primary">
-          <p>Click here to claim a free hat</p>
+        <div class="callout">
+          <p>Orange color lies between red and yellow on the light spectrum</p>
         </div>
       </div>
     </docs-item>
@@ -23,7 +23,7 @@
     <docs-item title="Basic" :code="callout" v-else>
       <div slot="body">
         <o-callout>
-          <p>Click here to claim a free hat</p>
+          <p>Orange color lies between red and yellow on the light spectrum</p>
         </o-callout>
       </div>
     </docs-item>
@@ -34,7 +34,7 @@
     <docs-item title="With a shadow" :code="callout_shadow_html" v-if="is_html">
       <div slot="body">
         <div class="callout callout--color-white callout--shadow">
-          <p>Click here to claim a free hat</p>
+          <p>Orange color lies between red and yellow on the light spectrum</p>
         </div>
       </div>
     </docs-item>
@@ -42,10 +42,105 @@
     <docs-item title="With a shadow" :code="callout_shadow" v-else>
       <div slot="body">
         <o-callout color="white" shadow>
-          <p>Click here to claim a free hat</p>
+          <p>Orange color lies between red and yellow on the light spectrum</p>
         </o-callout>
       </div>
     </docs-item>
+    
+
+
+
+    <!-- With action  -->
+    <docs-item title="With an action" :code="callout_action_html" v-if="is_html">
+      <div slot="body">
+        <div class="callout callout--color-green callout--action">
+          <p>Click here to claim a free hat</p>
+        </div>
+      </div>
+    </docs-item>
+
+
+
+
+    <!-- As stats -->
+    <docs-item title="As stats" :code="callout_stats_html" v-if="is_html">
+      <div slot="body">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="callout">
+              <h1>28K</h1>
+              <p>
+                <strong>Impressions</strong><br>
+                Quantity of notifications shown
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="callout">
+              <h1>285</h1>
+              <p>
+                <strong>Clicks</strong><br>
+                Traffic from linked notifications
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="callout">
+              <h1>1106</h1>
+              <p>
+                <strong>Engagements</strong><br>
+                Mouse hovers, which pause the animation
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="callout">
+              <h1>$724</h1>
+              <p>
+                <strong>Sales</strong><br>
+                Goal completion value in dollars
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </docs-item>
+
+
+
+
+    <!-- With an image -->
+    <docs-item title="With an image" :code="callout_image_html" v-if="is_html">
+      <div slot="body">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="callout callout--color-primary callout--action">
+              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/google-analytics.svg" alt="Callout #1"></div>
+              <p><strong>Conversion metrics</strong><br>
+                Connect Google Analytics to see Conversions &amp; Sales metrics.
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="callout callout callout--color-primary callout--action">
+              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/badges.svg" alt="Callout #3"></div>
+              <p><strong>Achievements</strong><br>
+                Take note on your wins. Receive Badges for every milestone.
+              </p>
+            </div>
+          </div>
+          <div class="col-sm-3">
+            <div class="callout callout callout--color-primary callout--action">
+              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/slack.svg" alt="Callout #4"></div>
+              <p><strong>Real-time updates</strong><br>
+                Get informed by sending Fomo events to any #channel.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </docs-item>
+    
 
 
 
@@ -77,169 +172,10 @@
             <p>Click here to claim a free hat</p>
           </div>
         </li>
-        <li>
-          <div class="callout callout--color-yellow callout--shadow">
-            <p>Click here to claim a free hat</p>
-          </div>
-        </li>
-        <li>
-          <div class="callout callout--color-green callout--shadow">
-            <p>Click here to claim a free hat</p>
-          </div>
-        </li>
-        <li>
-          <div class="callout callout--color-red callout--shadow">
-            <p>Click here to claim a free hat</p>
-          </div>
-        </li>
-        <li>
-          <div class="callout callout--color-red-light callout--shadow">
-            <p>Click here to claim a free hat</p>
-          </div>
-        </li>
-      </ul>
-    </docs-item>
-
-    <docs-item title="Colors" :code="callout_colors" v-else>
-      <ul slot="body" class="docs__simple-list">
-        <li>
-          <o-callout color="white" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="gray" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="black" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="primary" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="secondary" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="yellow" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="green" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
-        <li>
-          <o-callout color="red" shadow>
-            <p>Click here to claim a free hat</p>
-          </o-callout>
-        </li>
       </ul>
     </docs-item>
 
 
-
-    <!-- With an image -->
-    <docs-item title="With an image" :code="callout_image_html" v-if="is_html">
-      <div slot="body">
-        <div class="row">
-          <div class="col-sm-3">
-            <div class="callout callout--color-yellow callout--shadow">
-              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/google-analytics.svg" alt="Callout #1"></div>
-              <p><strong>Conversion metrics</strong><br>
-                Connect Google Analytics to see Conversions &amp; Sales metrics.
-              </p>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="callout callout--color-secondary callout--shadow">
-              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/scorecard.svg" alt="Callout #2"></div>
-              <p><strong>Fomo Scorecard</strong><br>
-                Set up goals to measure Fomo's impact on your business. Easy.
-              </p>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="callout callout--color-white callout--shadow">
-              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/badges.svg" alt="Callout #3"></div>
-              <p><strong>Achievements</strong><br>
-                Take note on your wins. Receive Badges for every milestone.
-              </p>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="callout callout--color-white callout--shadow">
-              <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/slack.svg" alt="Callout #4"></div>
-              <p><strong>Real-time updates</strong><br>
-                Get informed by sending Fomo events to any #channel.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </docs-item>
-
-    <docs-item title="With an image" :code="callout_image" v-else>
-      <div slot="body">
-        <div class="row">
-          <div class="col-sm-3">
-            <o-callout color="yellow" shadow>
-              <div class="callout__image">
-                <img src="https://fomo.com/static/images/app/dashboard/google-analytics.svg" alt="Callout #1">
-              </div>
-              <p>
-                <strong>Conversion metrics</strong><br>
-                Connect Google Analytics to see Conversions & Sales metrics.
-              </p>
-            </o-callout>
-          </div>
-
-          <div class="col-sm-3">
-            <o-callout color="secondary" shadow>
-              <div class="callout__image">
-                <img src="https://fomo.com/static/images/app/dashboard/scorecard.svg" alt="Callout #2">
-              </div>
-              <p>
-                <strong>Fomo Scorecard</strong><br>
-                Set up goals to measure Fomo's impact on your business. Easy.
-              </p>
-            </o-callout>
-          </div>
-
-          <div class="col-sm-3">
-            <o-callout color="white" shadow>
-              <div class="callout__image">
-                <img src="https://fomo.com/static/images/app/dashboard/badges.svg" alt="Callout #3">
-              </div>
-              <p>
-                <strong>Achievements</strong><br>
-                Take note on your wins. Receive Badges for every milestone.
-              </p>
-            </o-callout>
-          </div>
-
-          <div class="col-sm-3">
-            <o-callout color="white" shadow>
-              <div class="callout__image">
-                <img src="https://fomo.com/static/images/app/dashboard/slack.svg" alt="Callout #4">
-              </div>
-              <p>
-                <strong>Real-time updates</strong><br>
-                Get informed by sending Fomo events to any #channel.
-              </p>
-            </o-callout>
-          </div>
-        </div>
-      </div>
-    </docs-item>
 
     <!-- Usage  -->
     <docs-item title="Usage" v-if="is_html">
@@ -257,7 +193,7 @@
         <tbody>
         <tr>
           <td>color</td>
-          <td>Set with <code>.button--color-{color}</code></td>
+          <td>Set with <code>.callout--color-{color}</code></td>
           <td>See <a href="/colors">Colors</a> page</td>
           <td>String</td>
           <td>-</td>
@@ -265,8 +201,16 @@
         </tr>
         <tr>
           <td>shadow</td>
-          <td>Set with <code>.button--shadow</code></td>
-          <td><code>shadow</code></td>
+          <td>Throw a shadow</td>
+          <td><code>.callout--shadow</code></td>
+          <td>String</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>action</td>
+          <td>Show hover effects</td>
+          <td><code>.callout--action</code></td>
           <td>String</td>
           <td>-</td>
           <td>-</td>
@@ -302,6 +246,13 @@
           <td>-</td>
           <td>false</td>
         </tr>
+        <tr>
+          <td>action</td>
+          <td>Show hover effects</td>
+          <td>Boolean</td>
+          <td>-</td>
+          <td>false</td>
+        </tr>
         </tbody>
       </table>
     </docs-item>
@@ -316,107 +267,60 @@ export default {
   data () {
     return {
       is_html: true,
-      callout: `<o-callout>
-  <p>Click here to claim a free hat</p>
-</o-callout>`,
-      callout_html: `<div class="callout callout--color-primary">
-  <p>Click here to claim a free hat</p>
+      callout_html: `<div class="callout">
+  <p>Orange color lies between red and yellow on the light spectrum</p>
 </div>`,
 
-      callout_shadow: `<o-callout color="white" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>`,
       callout_shadow_html: `<div class="callout callout--color-white callout--shadow">
+  <p>Orange color lies between red and yellow on the light spectrum</p>
+</div>`,
+
+      callout_action_html: `<div class="callout callout--color-green callout--action">
   <p>Click here to claim a free hat</p>
 </div>`,
 
-      callout_colors: `<o-callout color="white" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="gray" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="black" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="primary" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="secondary" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="yellow" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="green" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>
-
-<o-callout color="red" shadow>
-  <p>Click here to claim a free hat</p>
-</o-callout>`,
-      callout_colors_html: `<div class="callout callout--color-{color} callout--shadow">
-  <p>Click here to claim a free hat</p>
-</div>`,
-
-      callout_image: `<div class="row">
+      callout_stats_html: `<div class="row">
   <div class="col-sm-3">
-    <o-callout color="yellow" shadow>
-      <div class="callout__image">
-        <img src="https://fomo.com/static/images/app/dashboard/google-analytics.svg" alt="Callout #1">
-      </div>
+    <div class="callout">
+      <h1>28K</h1>
       <p>
-        <strong>Conversion metrics</strong><br>
-        Connect Google Analytics to see Conversions & Sales metrics.
+        <strong>Impressions</strong><br>
+        Quantity of notifications shown
       </p>
-    </o-callout>
+    </div>
   </div>
-
   <div class="col-sm-3">
-    <o-callout color="secondary" shadow>
-      <div class="callout__image">
-        <img src="https://fomo.com/static/images/app/dashboard/scorecard.svg" alt="Callout #2">
-      </div>
+    <div class="callout">
+      <h1>285</h1>
       <p>
-        <strong>Fomo Scorecard</strong><br>
-        Set up goals to measure Fomo's impact on your business. Easy.
+        <strong>Clicks</strong><br>
+        Traffic from linked notifications
       </p>
-    </o-callout>
+    </div>
   </div>
-
   <div class="col-sm-3">
-    <o-callout color="white" shadow>
-      <div class="callout__image">
-        <img src="https://fomo.com/static/images/app/dashboard/badges.svg" alt="Callout #3">
-      </div>
+    <div class="callout">
+      <h1>1106</h1>
       <p>
-        <strong>Achievements</strong><br>
-        Take note on your wins. Receive Badges for every milestone.
+        <strong>Engagements</strong><br>
+        Mouse hovers, which pause the animation
       </p>
-    </o-callout>
+    </div>
   </div>
-
   <div class="col-sm-3">
-    <o-callout color="white" shadow>
-      <div class="callout__image">
-        <img src="https://fomo.com/static/images/app/dashboard/slack.svg" alt="Callout #4">
-      </div>
+    <div class="callout">
+      <h1>$724</h1>
       <p>
-        <strong>Real-time updates</strong><br>
-        Get informed by sending Fomo events to any #channel.
+        <strong>Sales</strong><br>
+        Goal completion value in dollars
       </p>
-    </o-callout>
+    </div>
   </div>
 </div>`,
+
       callout_image_html: `<div class="row">
   <div class="col-sm-3">
-    <div class="callout callout--color-yellow callout--shadow">
+    <div class="callout callout--color-primary callout--action">
       <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/google-analytics.svg" alt="Callout #1"></div>
       <p><strong>Conversion metrics</strong><br>
         Connect Google Analytics to see Conversions &amp; Sales metrics.
@@ -424,15 +328,7 @@ export default {
     </div>
   </div>
   <div class="col-sm-3">
-    <div class="callout callout--color-secondary callout--shadow">
-      <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/scorecard.svg" alt="Callout #2"></div>
-      <p><strong>Fomo Scorecard</strong><br>
-        Set up goals to measure Fomo's impact on your business. Easy.
-      </p>
-    </div>
-  </div>
-  <div class="col-sm-3">
-    <div class="callout callout--color-white callout--shadow">
+    <div class="callout callout callout--color-primary callout--action">
       <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/badges.svg" alt="Callout #3"></div>
       <p><strong>Achievements</strong><br>
         Take note on your wins. Receive Badges for every milestone.
@@ -440,7 +336,7 @@ export default {
     </div>
   </div>
   <div class="col-sm-3">
-    <div class="callout callout--color-white callout--shadow">
+    <div class="callout callout callout--color-primary callout--action">
       <div class="callout__image"><img src="https://fomo.com/static/images/app/dashboard/slack.svg" alt="Callout #4"></div>
       <p><strong>Real-time updates</strong><br>
         Get informed by sending Fomo events to any #channel.
