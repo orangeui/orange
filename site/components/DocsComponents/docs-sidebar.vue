51c2dd4782b04097d426a74dcc4329966f27557f
Jan Perse <jan.perse@gmail.com>
2019-08-22 08:05:32 +0200

    Update/Docs Sidebar (#12)
    * Add docs sidebar component, update docs sidebar style
    
    * Make sidebar menu collapsible and update folder/route structure

---
+++ site/components/DocsComponents/docs-sidebar.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="docs__sidebar-wrap">
+    <aside class="docs__sidebar">
+      <router-link :to="{ name: 'docs'}" tag="h5">Getting started</router-link>
+
+      <ul>
+        <li><router-link :to="{ name: 'docs'}" exact><span>Install</span></router-link></li>
+      </ul>
+
+      <router-link :to="{ name: 'docs-global'}" tag="h5">Global</router-link>
+
+      <ul>
+        <li><router-link :to="{ name: 'docs-global'}" exact><span>Colors</span></router-link></li>
+        <li><router-link :to="{ name: 'docs-global-icons'}"><span>Icons</span></router-link></li>
+        <li><router-link :to="{ name: 'docs-global-materials'}"><span>Materials</span></router-link></li>
+        <li><router-link :to="{ name: 'docs-global-spacings'}"><span>Spacings</span></router-link></li>
+      </ul>
+
+      <router-link :to="{ name: 'docs-typography'}" tag="h5">Typography</router-link>
+
+      <ul>
+        <li><router-link :to="{ name: 'docs-typography'}" exact><span>Font</span></router-link></li>
+        <li><router-link :to="{ name: 'docs-typography-sizes'}"><span>Sizes</span></router-link></li>
+        <li><router-link :to="{ name: 'docs-typography-styles'}"><span>Styles</span></router-link></li>
+      </ul>
+
+      <router-link :to="{ name: 'docs-components-basic'}" tag="h5">Components</router-link>
+
+      <ul>
+        <li>
+          <router-link :to="{ name: 'docs-components-basic'}" tag="h6">Basic</router-link>
+
+          <ul>
+            <li><router-link :to="{ name: 'docs-components-basic'}" exact><span>Button</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-basic-label'}"><span>Label</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-basic-list'}"><span>List</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-basic-navigation'}"><span>Navigation</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-basic-table'}"><span>Table</span></router-link></li>
+          </ul>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'docs-components-custom'}" tag="h6">Custom</router-link>
+
+          <ul>
+            <li><router-link :to="{ name: 'docs-components-custom'}" exact><span>Avatar</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-custom-breadcrumbs'}"><span>Breadcrumbs</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-custom-callout'}"><span>Callout</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-custom-card'}"><span>Card</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-custom-empty-state'}"><span>Empty State</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-custom-message'}"><span>Message</span></router-link></li>
+          </ul>
+        </li>
+
+        <li>
+          <router-link :to="{ name: 'docs-components-form'}" tag="h6">Forms</router-link>
+
+          <ul>
+            <li><router-link :to="{ name: 'docs-components-form'}" exact><span>Checkbox</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-form-dropdown'}"><span>Dropdown</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-form-input'}"><span>Input</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-form-radio'}"><span>Radio</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-form-textarea'}"><span>Textarea</span></router-link></li>
+            <li><router-link :to="{ name: 'docs-components-form-toggle'}"><span>Toggle</span></router-link></li>
+          </ul>
+        </li>
+      </ul>
+    </aside>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DocsSidebar'
+  }
+</script>
