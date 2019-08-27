650f5e271cd777388edf7c203bd4ee7bad434fc2
Jan Perse <jan@elpy.io>
2019-08-27 09:18:47 +0200

    Simplify toggle class ifs in textarea

--- src/js/src/textarea.js
+++ src/js/src/textarea.js
@@ -6,7 +6,7 @@
  */
 
 const Textarea = (function() {
-  
+
   /**
   * ------------------------------------------------------------------------
   * Constants
@@ -41,7 +41,7 @@ const Textarea = (function() {
 
       if (typeof textarea.value === "string" && textarea.value !== ''){
         textarea.classList.add(Classes._active_class);
-      } 
+      }
 
       // textarea.addEventListener("focusin", toggleActiveClass, false);
       // textarea.addEventListener("focusout", toggleActiveClass, false);
@@ -50,14 +50,19 @@ const Textarea = (function() {
       // if (auto_expand_enabled){
       //   textarea.addEventListener("input", autoExpandTextarea, false);
       // }
-    }    
-  }  
-  
+    }
+  }
+
   // Function to toggle active class
   function toggleActiveClass(evt) {
     let textarea = evt.target;
     let textarea_has_text = textarea.value !== "" && textarea.value !== null;
-    if (textarea_has_text) {
+    if (textarea_has_text || evt.type === "focusin") {
+      textarea.classList.add(Classes._active_class);
+    } else if (evt.type === "focusout" && !textarea_has_text || !textarea_has_text) {
+      textarea.classList.remove(Classes._active_class);
+    }
+    /*if (textarea_has_text) {
       textarea.classList.add(Classes._active_class);
     } else {
       textarea.classList.remove(Classes._active_class);
@@ -67,7 +72,7 @@ const Textarea = (function() {
     }
     if (evt.type === "focusout" && !textarea_has_text) {
       textarea.classList.remove(Classes._active_class);
-    }
+    }*/
   }
 
   // Function to add posibility for auto expanding textarea
@@ -95,12 +100,12 @@ const Textarea = (function() {
     autoExpandTextarea: autoExpandTextarea,
     auto_expand: Selectors.auto_expand
   }
-  
+
 })();
 
 // On init run activateTextareas if they exist
 if (Textarea.textareas.length) {
-  Textarea.activateTextareas();  
+  Textarea.activateTextareas();
 }
 
 // Add focusin event
