7d04517be925fd29db21a68625980d819f339fa5
Jan Perse <jan.perse@gmail.com>
2019-09-11 08:52:38 +0200

    Feature/Grid Basic (#31)
    * Update grid variables and add basic map of breakpoints
    
    * Remove unneeded docs css
    
    * Add grid to sidebar
    
    * Update grid variables, start with css grid
    
    * Add first example of grid and add new section to sidebar called Layout
    
    * Remove grid link from Global category
    
    * Add first iteration of grid column layout
    
    * Add auto fill grid and update examples
    
    * Add mixin for auto fill columns
    
    * Simplify the use of setting column width and set fixed 12 column system
    
    * Fix examples
    
    * Add breakpoint function and breakpoint-down mixin
    
    * Fix grid in callout
    
    * Fix docs css and callout example

---
+++ site/pages/docs/layout/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="docs__section">
+    <div class="docs__header">
+      <h1>Grid</h1>
+      <o-label size="small" text="HTML"/>
+    </div>
+
+    <docs-item class="explanation">
+      <div slot="body">
+        Description of Grid system
+      </div>
+    </docs-item>
+
+    <docs-item title="Container" :css_code="container_widths">
+      <div slot="body">
+        <p>
+          Container is an element that wraps content and has gutter on sides. Class <code>.container</code> limits the
+          width, while <code>.container-fluid</code> is full width.
+          <br>
+          Gutter can be changed with variable <code>$container-gutter</code>. Default is <code>12px</code>
+        </p>
+
+        <p>Default container widths on all the breakpoints are shown below.</p>
+      </div>
+    </docs-item>
+
+    <docs-item title="Auto Fill column" :code="equal_column_width">
+      <div slot="body">
+        <p>
+          By default, <code>.grid</code> is using 12 column system with auto responsive columns.
+          <br>
+          All the columns are equal width and you can put as many columns in a grid as you want and columns will be moved to a new row when there is not enough space in a container.
+        </p>
+
+        <h3>Narrow</h3>
+        <div class="grid grid--columns-auto-narrow">
+          <div v-for="number in 6" :key="number">
+            {{ number }}
+          </div>
+        </div>
+
+        <h3>Regular (default)</h3>
+        <div class="grid grid--columns-auto">
+          <div v-for="number in 6" :key="number">
+            {{ number }}
+          </div>
+        </div>
+
+        <h3>Wide</h3>
+        <div class="grid grid--columns-auto-wide">
+          <div v-for="number in 6" :key="number">
+            {{ number }}
+          </div>
+        </div>
+      </div>
+    </docs-item>
+
+    <docs-item title="Different column width" :code="different_column_width">
+      <div slot="body">
+        <p>
+          We achieve width of columns with choosing positions rather than setting exact width to columns.
+          We still limit to 12 column grid system, so setting widths of the columns is only available till 12.
+        </p>
+
+        <div class="grid grid--columns-narrow">
+          <div class="col-6">2</div>
+          <div class="col-6">2</div>
+          <div class="col-4">1</div>
+          <div class="col-3">1</div>
+          <div class="col-5">2</div>
+          <div class="col-1">2</div>
+          <div class="col-2">2</div>
+          <div class="col-7">2</div>
+          <div class="col-2">2</div>
+        </div>
+      </div>
+    </docs-item>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DocsGrid',
+
+    data () {
+      return {
+        // TODO: Change this value with actual container variable instead of breakpoint
+        container_widths: `$breakpoints: (
+  xxsmall: 320px,
+  xsmall: 480px,
+  small: 640px,
+  medium: 768px,
+  large: 992px,
+  xlarge: 1200px,
+  xxlarge: 1440px,
+) !default;`,
+        equal_column_width: `<div class="grid grid--columns-auto-narrow">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+</div>
+
+<div class="grid">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+</div>
+
+<div class="grid grid--columns-auto-wide">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
+  <div>5</div>
+  <div>6</div>
+</div>`,
+        different_column_width: `<div class="grid">
+  <div class="col-6">2</div>
+  <div class="col-6">2</div>
+  <div class="col-4">1</div>
+  <div class="col-3">1</div>
+  <div class="col-5">2</div>
+  <div class="col-1">2</div>
+  <div class="col-2">2</div>
+  <div class="col-7">2</div>
+  <div class="col-2">2</div>
+</div>`
+      }
+    }
+  }
+</script>
