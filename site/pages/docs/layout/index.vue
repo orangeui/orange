20702de7950a661a99e9feb73b9b33efc81a9553
Jan Perse <jan.perse@gmail.com>
2019-09-13 08:35:36 +0200

    Feature/Grid Column Positions (#36)
    * Add basic column positioning
    
    * Add grid span mixin and update example
    
    * Add exact row and column positioning and update examples
    
    * Create a better example for columns with different positions
    
    * Add description of grid
    
    * Update variables $gutter to $gap for better understanding

--- site/pages/docs/layout/index.vue
+++ site/pages/docs/layout/index.vue
@@ -7,17 +7,19 @@
 
     <docs-item class="explanation">
       <div slot="body">
-        Description of Grid system
+        <p>
+          This grid is non-responsive until Orange <code>v1.4.0</code>.
+        </p>
       </div>
     </docs-item>
 
     <docs-item title="Container" :css_code="container_widths">
       <div slot="body">
         <p>
-          Container is an element that wraps content and has gutter on sides. Class <code>.container</code> limits the
+          Container is an element that wraps content and has gap on sides. Class <code>.container</code> limits the
           width, while <code>.container-fluid</code> is full width.
           <br>
-          Gutter can be changed with variable <code>$container-gutter</code>. Default is <code>12px</code>
+          Gutter can be changed with variable <code>$container-gap</code>. Default is <code>12px</code>
         </p>
 
         <p>Default container widths on all the breakpoints are shown below.</p>
@@ -57,21 +59,54 @@
 
     <docs-item title="Different column width" :code="different_column_width">
       <div slot="body">
+        <div class="grid">
+          <div class="col-6">1</div>
+          <div class="col-6">2</div>
+          <div class="col-4">3</div>
+          <div class="col-3">4</div>
+          <div class="col-5">5</div>
+          <div class="col-1">6</div>
+          <div class="col-2">7</div>
+          <div class="col-7">8</div>
+          <div class="col-2">9</div>
+        </div>
+      </div>
+    </docs-item>
+
+    <docs-item title="Columns with different positions" :code="position_column">
+      <div slot="body">
         <p>
-          We achieve width of columns with choosing positions rather than setting exact width to columns.
-          We still limit to 12 column grid system, so setting widths of the columns is only available till 12.
+          By setting class like <code>.col-7/12</code>, column will span from column 1 to including 7, but you have to set in which row like <code>.row-2</code>.
+          <br>
+          You can also change positions of columns with this, check the example below.
         </p>
 
-        <div class="grid grid--columns-narrow">
-          <div class="col-6">2</div>
-          <div class="col-6">2</div>
-          <div class="col-4">1</div>
+        <div class="grid">
           <div class="col-3">1</div>
-          <div class="col-5">2</div>
-          <div class="col-1">2</div>
-          <div class="col-2">2</div>
-          <div class="col-7">2</div>
-          <div class="col-2">2</div>
+          <div class="col-9">2</div>
+          <div class="col-1">3</div>
+          <div class="col-5">4</div>
+          <div class="col-2/7 row-pos-2">5</div>
+          <div class="col-5/10 row-pos-3">6</div>
+          <div class="col-4">7</div>
+          <div class="col-2">8</div>
+        </div>
+      </div>
+    </docs-item>
+
+    <docs-item title="Spanning column over multiple rows" :code="span_column_over_rows">
+      <div slot="body">
+        <div class="grid">
+          <div class="col-3">1</div>
+          <div class="col-3">2</div>
+          <div class="col-6">3</div>
+          <div class="col-8 row-3">4</div>
+          <div class="col-4">5</div>
+          <div class="col-4">8</div>
+          <div class="col-4">8</div>
+          <div class="col-4">8</div>
+          <div class="col-4">8</div>
+          <div class="col-4">8</div>
         </div>
       </div>
     </docs-item>
@@ -130,6 +165,26 @@
   <div class="col-2">2</div>
   <div class="col-7">2</div>
   <div class="col-2">2</div>
+</div>`,
+        position_column: `<div class="grid">
+  <div class="col-3">1</div>
+  <div class="col-9">2</div>
+  <div class="col-5">3</div>
+  <div class="col-1/7 row-pos-2">4</div>
+  <div class="col-5/12 row-pos-3">5</div>
+  <div class="col-4">6</div>
+</div>`,
+        span_column_over_rows: `<div class="grid">
+  <div class="col-3">1</div>
+  <div class="col-3">2</div>
+  <div class="col-6">3</div>
+  <div class="col-8 row-3">4</div>
+  <div class="col-4">5</div>
+  <div class="col-4">8</div>
+  <div class="col-4">8</div>
+  <div class="col-4">8</div>
+  <div class="col-4">8</div>
+  <div class="col-4">8</div>
 </div>`
       }
     }
