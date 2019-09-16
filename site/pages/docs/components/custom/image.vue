b97977e611166dd68802eb2ef9afcf6435550d22
matevzgolavsek <matevz.golavsek@gmail.com>
2019-09-16 16:39:48 +0200

    Add full path to images for examples

--- site/pages/docs/components/custom/image.vue
+++ site/pages/docs/components/custom/image.vue
@@ -16,10 +16,10 @@
       <div slot="body">
         <p>Apply classes directly to <code>img</code> tag or on <code>a</code> tag.</p>
         
-        <img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
+        <img class="image" src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
 
         <a href="#" class="image">
-          <img src="/images/thumbnails/fox.jpg" alt="Foxy" />
+          <img src="https://orangecss.com/images/thumbnails/fox.jpg" alt="Foxy" />
         </a>
       </div>
     </docs-item>
@@ -27,7 +27,7 @@
     <!-- With shadow  -->
     <docs-item title="With shadow" :code="thumbnail_shadow" v-if="is_html">
       <div slot="body">
-        <img class="image image--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+        <img class="image image--shadow" src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
       </div>
     </docs-item>
 
@@ -38,10 +38,10 @@
           Add border radius to image.
         </p>
 
-        <img class="image image--rounded" src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
+        <img class="image image--rounded" src="https://orangecss.com/images/thumbnails/square/cat-square.jpg" alt="Katze" />
 
         <a href="#" class="image image--size-small image--rounded image--action">
-          <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+          <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
         </a>
       </div>
     </docs-item>
@@ -53,10 +53,10 @@
           Please note that images have to be square for this style to work as expected.
         </p>
 
-        <img class="image image--circle" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+        <img class="image image--circle" src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
 
         <a href="#" class="image image--size-small image--circle image--action">
-          <img src="/images/thumbnails/square/fox-square.jpg" alt="Foxy" />
+          <img src="https://orangecss.com/images/thumbnails/square/fox-square.jpg" alt="Foxy" />
         </a>
       </div>
     </docs-item>
@@ -66,24 +66,24 @@
       <ul slot="body" class="docs__simple-list">
         <li>
           <p>Default - auto size</p>
-          <img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
+          <img class="image" src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
         </li>
         <li>
           <p>Extra small - 60px</p>
-          <img class="image image--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
+          <img class="image image--size-xsmall" src="https://orangecss.com/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
         </li>
         <li>
           <p>Small - 120px</p>
-          <img class="image image--size-small image--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
+          <img class="image image--size-small image--action" src="https://orangecss.com/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
         </li>
         <li>
           <p>Medium - 240px</p>
 
-          <img class="image image--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
+          <img class="image image--size-medium" src="https://orangecss.com/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
         </li>
         <li>
           <p>Large - 480px</p>
-          <img class="image image--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
+          <img class="image image--size-large" src="https://orangecss.com/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
         </li>
       </ul>
     </docs-item>
@@ -95,34 +95,34 @@
 
           <div class="col-2">
             <a href="#" class="image image--action">
-              <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+              <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
             </a>
           </div>
           <div class="col-2">
             <a href="#" class="image image--action">
-              <img src="/images/thumbnails/cat.jpg" alt="Katze" />
+              <img src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
             </a>
           </div>
           
           <div class="col-2">
             <a href="#" class="image image--action">
-              <img src="/images/thumbnails/square/dog-square.jpg" alt="Katze" />
+              <img src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Katze" />
             </a>
           </div>
           <div class="col-2">
             <a href="#" class="image image--action">
-              <img src="/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
+              <img src="https://orangecss.com/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
             </a>
           </div>
 
           <div class="col-2">
             <a href="#" class="image image--circle image--action">
-              <img src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
+              <img src="https://orangecss.com/images/thumbnails/square/cat-square.jpg" alt="Katze" />
             </a>
           </div>
           <div class="col-2">
             <a href="#" class="image image--circle image--action">
-              <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
+              <img src="https://orangecss.com/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
             </a>
           </div>
          
@@ -137,25 +137,25 @@
 
         <div class="image-wrapper">
           <div class="image">
-            <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+            <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
           </div>
         </div>
 
         <div class="image-wrapper image-wrapper--size-xsmall">
           <div class="image">
-            <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+            <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
           </div>
         </div>
 
         <div class="image-wrapper image-wrapper--size-small">
           <div class="image image--circle">
-            <img src="/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
+            <img src="https://orangecss.com/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
           </div>
         </div>
 
         <div class="image-wrapper image-wrapper--size-medium">
           <div class="image image--circle">
-            <img src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
+            <img src="https://orangecss.com/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
           </div>
         </div>
 
@@ -164,28 +164,28 @@
           <div class="col-2">
             <div class="image-wrapper">
               <a href="#" class="image image--rounded image--action">
-                <img src="/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
+                <img src="https://orangecss.com/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
               </a>
             </div>
           </div>
           <div class="col-2">
             <div class="image-wrapper">
               <a href="#" class="image image--rounded image--action">
-                <img src="/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
+                <img src="https://orangecss.com/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
               </a>
             </div>
           </div>
           <div class="col-2">
             <div class="image-wrapper">
               <a href="#" class="image image--circle image--action">
-                <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
+                <img src="https://orangecss.com/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
               </a>
             </div>
           </div>
           <div class="col-2">
             <div class="image-wrapper">
               <a href="#" class="image image--size-large image--circle image--action">
-                <img src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+                <img src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
               </a>
             </div>
           </div>
@@ -262,70 +262,70 @@ export default {
   data () {
     return {
       is_html: true,
-      thumbnail_basic: `<img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
-<a href="#" class="image"><img src="/images/thumbnails/fox.jpg" alt="Foxy" /></a>`,
-      thumbnail_shadow: `<img class="image image--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />`,
-      thumbnail_rounded: `<img class="image image--rounded" src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
+      thumbnail_basic: `<img class="image" src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
+<a href="#" class="image"><img src="https://orangecss.com/images/thumbnails/fox.jpg" alt="Foxy" /></a>`,
+      thumbnail_shadow: `<img class="image image--shadow" src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />`,
+      thumbnail_rounded: `<img class="image image--rounded" src="https://orangecss.com/images/thumbnails/square/cat-square.jpg" alt="Katze" />
 <a href="#" class="image image--size-small image--rounded image--action">
-  <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+  <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
 </a>`,
-      thumbnail_circle: `<img class="image image--circle" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
-<a href="#" class="image image--size-small image--circle image--action"><img src="/images/thumbnails/square/fox-square.jpg" alt="Foxy" /></a>`,
-      thumbnail_sizes: `<img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
-<img class="image image--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
-<img class="image image--size-small image--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
-<img class="image image--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
-<img class="image image--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />`,
+      thumbnail_circle: `<img class="image image--circle" src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+<a href="#" class="image image--size-small image--circle image--action"><img src="https://orangecss.com/images/thumbnails/square/fox-square.jpg" alt="Foxy" /></a>`,
+      thumbnail_sizes: `<img class="image" src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
+<img class="image image--size-xsmall" src="https://orangecss.com/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
+<img class="image image--size-small image--action" src="https://orangecss.com/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
+<img class="image image--size-medium" src="https://orangecss.com/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
+<img class="image image--size-large" src="https://orangecss.com/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />`,
       thumbnail_action: `<div class="grid">
   <div class="col-2">
     <a href="#" class="image image--action">
-      <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+      <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
     </a>
   </div>
   <div class="col-2">
     <a href="#" class="image image--action">
-      <img src="/images/thumbnails/cat.jpg" alt="Katze" />
+      <img src="https://orangecss.com/images/thumbnails/cat.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
     <a href="#" class="image image--action">
-      <img src="/images/thumbnails/square/dog-square.jpg" alt="Katze" />
+      <img src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
     <a href="#" class="image image--action">
-      <img src="/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
+      <img src="https://orangecss.com/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
     </a>
   </div>
   <div class="col-2">
     <a href="#" class="image image--circle image--action">
-      <img src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
+      <img src="https://orangecss.com/images/thumbnails/square/cat-square.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
     <a href="#" class="image image--circle image--action">
-      <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
+      <img src="https://orangecss.com/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
     </a>
   </div>
 </div>`,
       thumbnail_dimensions: `<div class="image-wrapper">
   <div class="image">
-    <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+    <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
   </div>
 </div>
 <div class="image-wrapper image-wrapper--size-xsmall">
   <div class="image">
-    <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+    <img src="https://orangecss.com/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
   </div>
 </div>
 <div class="image-wrapper image-wrapper--size-small">
   <div class="image image--circle">
-    <img src="/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
+    <img src="https://orangecss.com/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
   </div>
 </div>
 <div class="image-wrapper image-wrapper--size-medium">
   <div class="image image--circle">
-    <img src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
+    <img src="https://orangecss.com/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
   </div>
 </div>
 
@@ -333,28 +333,28 @@ export default {
   <div class="col-2">
     <div class="image-wrapper">
       <a href="#" class="image image--rounded image--action">
-        <img src="/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
+        <img src="https://orangecss.com/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
       </a>
     </div>
   </div>
   <div class="col-2">
     <div class="image-wrapper">
       <a href="#" class="image image--rounded image--action">
-        <img src="/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
+        <img src="https://orangecss.com/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
       </a>
     </div>
   </div>
   <div class="col-2">
     <div class="image-wrapper">
       <a href="#" class="image image--circle image--action">
-        <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
+        <img src="https://orangecss.com/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
       </a>
     </div>
   </div>
   <div class="col-2">
     <div class="image-wrapper">
       <a href="#" class="image image--size-large image--circle image--action">
-        <img src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+        <img src="https://orangecss.com/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
       </a>
     </div>
   </div>
