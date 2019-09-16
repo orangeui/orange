f92bb388f1ea934b6336ce9e19505c689ea523e5
matevzgolavsek <matevz.golavsek@gmail.com>
2019-09-16 13:38:25 +0200

    Rename thumbnail to image

--- site/pages/docs/components/custom/image.vue
+++ site/pages/docs/components/custom/image.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="docs__section">
     <div class="docs__header">
-      <h1>Thumbnail</h1>
+      <h1>Image</h1>
       <o-label size="small" text="HTML" />
     </div>
 
     <docs-item class="explanation">
       <div slot="body">
-        <p>Thumbnail is a styled image which can be combined to gallery.</p>
+        <p>Add styling to images.</p>
       </div>
     </docs-item>
 
@@ -16,9 +16,9 @@
       <div slot="body">
         <p>Apply classes directly to <code>img</code> tag or on <code>a</code> tag.</p>
         
-        <img class="thumbnail" src="/images/thumbnails/cat.jpg" alt="Katze" />
+        <img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
 
-        <a href="#" class="thumbnail">
+        <a href="#" class="image">
           <img src="/images/thumbnails/fox.jpg" alt="Foxy" />
         </a>
       </div>
@@ -27,7 +27,7 @@
     <!-- With shadow  -->
     <docs-item title="With shadow" :code="thumbnail_shadow" v-if="is_html">
       <div slot="body">
-        <img class="thumbnail thumbnail--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
+        <img class="image image--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
       </div>
     </docs-item>
 
@@ -38,9 +38,9 @@
           Please note that images have to be square for this style to work as expected.
         </p>
 
-        <img class="thumbnail thumbnail--circle" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+        <img class="image image--circle" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
 
-        <a href="#" class="thumbnail thumbnail--size-small thumbnail--circle thumbnail--action">
+        <a href="#" class="image image--size-small image--circle image--action">
           <img src="/images/thumbnails/square/fox-square.jpg" alt="Foxy" />
         </a>
       </div>
@@ -51,24 +51,24 @@
       <ul slot="body" class="docs__simple-list">
         <li>
           <p>Default - auto size</p>
-          <img class="thumbnail" src="/images/thumbnails/cat.jpg" alt="Doggo" />
+          <img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
         </li>
         <li>
           <p>Extra small - 60px</p>
-          <img class="thumbnail thumbnail--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
+          <img class="image image--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
         </li>
         <li>
           <p>Small - 120px</p>
-          <img class="thumbnail thumbnail--size-small thumbnail--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
+          <img class="image image--size-small image--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
         </li>
         <li>
           <p>Medium - 240px</p>
 
-          <img class="thumbnail thumbnail--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
+          <img class="image image--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
         </li>
         <li>
           <p>Large - 480px</p>
-          <img class="thumbnail thumbnail--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
+          <img class="image image--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
         </li>
       </ul>
     </docs-item>
@@ -79,34 +79,34 @@
         <div class="grid">
 
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--action">
+            <a href="#" class="image image--action">
               <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
             </a>
           </div>
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--size-auto thumbnail--action">
+            <a href="#" class="image image--action">
               <img src="/images/thumbnails/cat.jpg" alt="Katze" />
             </a>
           </div>
           
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--action">
+            <a href="#" class="image image--action">
               <img src="/images/thumbnails/square/dog-square.jpg" alt="Katze" />
             </a>
           </div>
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--action">
+            <a href="#" class="image image--action">
               <img src="/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
             </a>
           </div>
 
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+            <a href="#" class="image image--circle image--action">
               <img src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
             </a>
           </div>
           <div class="col-2">
-            <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+            <a href="#" class="image image--circle image--action">
               <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
             </a>
           </div>
@@ -118,28 +118,28 @@
     <!-- Any dimension in square or circle  -->
     <docs-item title="Any dimension in square or circle" :code="thumbnail_dimensions" v-if="is_html">
       <div slot="body">
-        <p>The structure of HTML is different as additional wrappers are needed to achieve the result. To change size of this thumbnails apply class to <code>.thumbnail-wrapper</code> like <code>.thumbnail-wrapper--size-{size}</code></p>
+        <p>The structure of HTML is different as additional wrappers are needed to achieve the result. To change size of this images apply class to <code>.image-wrapper</code> like <code>.image-wrapper--size-{size}</code></p>
 
-        <div class="thumbnail-wrapper">
-          <div class="thumbnail">
+        <div class="image-wrapper">
+          <div class="image">
             <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
           </div>
         </div>
 
-        <div class="thumbnail-wrapper thumbnail-wrapper--size-xsmall">
-          <div class="thumbnail">
+        <div class="image-wrapper image-wrapper--size-xsmall">
+          <div class="image">
             <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
           </div>
         </div>
 
-        <div class="thumbnail-wrapper thumbnail-wrapper--size-small">
-          <div class="thumbnail thumbnail--circle">
+        <div class="image-wrapper image-wrapper--size-small">
+          <div class="image image--circle">
             <img src="/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
           </div>
         </div>
 
-        <div class="thumbnail-wrapper thumbnail-wrapper--size-medium">
-          <div class="thumbnail thumbnail--circle">
+        <div class="image-wrapper image-wrapper--size-medium">
+          <div class="image image--circle">
             <img src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
           </div>
         </div>
@@ -147,31 +147,31 @@
         <h3>In grid</h3>
         <div class="grid">
           <div class="col-2">
-            <div class="thumbnail-wrapper">
-              <a href="#" class="thumbnail thumbnail--action">
+            <div class="image-wrapper">
+              <a href="#" class="image image--action">
                 <img src="/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
               </a>
             </div>
           </div>
 
           <div class="col-2">
-            <div class="thumbnail-wrapper">
-              <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+            <div class="image-wrapper">
+              <a href="#" class="image image--circle image--action">
                 <img src="/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
               </a>
             </div>
           </div>
           <div class="col-2">
-            <div class="thumbnail-wrapper">
-              <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+            <div class="image-wrapper">
+              <a href="#" class="image image--circle image--action">
                 <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
               </a>
             </div>
           </div>
          
           <div class="col-2">
-            <div class="thumbnail-wrapper">
-              <a href="#" class="thumbnail thumbnail--size-large thumbnail--circle thumbnail--action">
+            <div class="image-wrapper">
+              <a href="#" class="image image--size-large image--circle image--action">
                 <img src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
               </a>
             </div>
@@ -198,7 +198,7 @@
         <tr>
           <td>shadow</td>
           <td>Throw a shadow</td>
-          <td><code>.thumbnail--shadow</code></td>
+          <td><code>.image--shadow</code></td>
           <td>String</td>
           <td>-</td>
           <td>-</td>
@@ -206,7 +206,7 @@
         <tr>
           <td>circle</td>
           <td>Add 100% border radius.</td>
-          <td><code>.thumbnail--circle</code></td>
+          <td><code>.image--circle</code></td>
           <td>String</td>
           <td>-</td>
           <td>-</td>
@@ -214,14 +214,14 @@
         <tr>
           <td>action</td>
           <td>Add hover effect</td>
-          <td><code>.thumbnail--action</code></td>
+          <td><code>.image--action</code></td>
           <td>String</td>
           <td>-</td>
           <td>-</td>
         </tr>
         <tr>
           <td>size</td>
-          <td>Set with <code>.thumbnail--size-{size}</code>, if you are using any dimension thumbnail you need to set it to <code>.thumbnail-wrapper--size-{size}</code></td>
+          <td>Set with <code>.image--size-{size}</code>, if you are using any dimension thumbnail you need to set it to <code>.image-wrapper--size-{size}</code></td>
           <td><code>xsmall</code>, <code>small</code>, <code>medium</code>, <code>large</code></td>
           <td>String</td>
           <td>-</td>
@@ -241,92 +241,94 @@ export default {
   data () {
     return {
       is_html: true,
-      thumbnail_basic: `<img class="thumbnail" src="/images/thumbnails/cat.jpg" alt="Katze" />`,
-      thumbnail_shadow: `<img class="thumbnail thumbnail--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />`,
-      thumbnail_circle: `<img class="thumbnail thumbnail--circle thumbnail--shadow" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />`,
-      thumbnail_sizes: `<img class="thumbnail" src="/images/thumbnails/cat.jpg" alt="Doggo" />
-<img class="thumbnail thumbnail--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
-<img class="thumbnail thumbnail--size-small thumbnail--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
-<img class="thumbnail thumbnail--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
-<img class="thumbnail thumbnail--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />`,
+      thumbnail_basic: `<img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
+<a href="#" class="image"><img src="/images/thumbnails/fox.jpg" alt="Foxy" /></a>`,
+      thumbnail_shadow: `<img class="image image--shadow" src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />`,
+      thumbnail_circle: `<img class="image image--circle" src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
+<a href="#" class="image image--size-small image--circle image--action"><img src="/images/thumbnails/square/fox-square.jpg" alt="Foxy" /></a>`,
+      thumbnail_sizes: `<img class="image" src="/images/thumbnails/cat.jpg" alt="Katze" />
+<img class="image image--size-xsmall" src="/images/thumbnails/width-60/fox-60.jpg" alt="Foxy" />
+<img class="image image--size-small image--action" src="/images/thumbnails/width-120/hedgehog-120.jpg" alt="Hedgy" />
+<img class="image image--size-medium" src="/images/thumbnails/width-240/dog-240.jpg" alt="Doggo" />
+<img class="image image--size-large" src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />`,
       thumbnail_action: `<div class="grid">
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--action">
+    <a href="#" class="image image--action">
       <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
     </a>
   </div>
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--size-auto thumbnail--action">
+    <a href="#" class="image image--action">
       <img src="/images/thumbnails/cat.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--action">
+    <a href="#" class="image image--action">
       <img src="/images/thumbnails/square/dog-square.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--action">
+    <a href="#" class="image image--action">
       <img src="/images/thumbnails/square/fox-square.jpg" alt="Doggo" />
     </a>
   </div>
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+    <a href="#" class="image image--circle image--action">
       <img src="/images/thumbnails/square/cat-square.jpg" alt="Katze" />
     </a>
   </div>
   <div class="col-2">
-    <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+    <a href="#" class="image image--circle image--action">
       <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
     </a>
   </div>
 </div>`,
-      thumbnail_dimensions: `<div class="thumbnail-wrapper">
-  <div class="thumbnail">
+      thumbnail_dimensions: `<div class="image-wrapper">
+  <div class="image">
     <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
   </div>
 </div>
-<div class="thumbnail-wrapper thumbnail-wrapper--size-xsmall">
-  <div class="thumbnail">
+<div class="image-wrapper image-wrapper--size-xsmall">
+  <div class="image">
     <img src="/images/thumbnails/hedgehog.jpg" alt="Hedgy" />
   </div>
 </div>
-<div class="thumbnail-wrapper thumbnail-wrapper--size-small">
-  <div class="thumbnail thumbnail--circle">
+<div class="image-wrapper image-wrapper--size-small">
+  <div class="image image--circle">
     <img src="/images/thumbnails/width-240/hedgehog-240.jpg" alt="Hedgy" />
   </div>
 </div>
-<div class="thumbnail-wrapper thumbnail-wrapper--size-medium">
-  <div class="thumbnail thumbnail--circle">
+<div class="image-wrapper image-wrapper--size-medium">
+  <div class="image image--circle">
     <img src="/images/thumbnails/width-480/hedgehog-480.jpg" alt="Hedgy" />
   </div>
 </div>
 
 <div class="grid">
   <div class="col-2">
-    <div class="thumbnail-wrapper">
-      <a href="#" class="thumbnail thumbnail--action">
+    <div class="image-wrapper">
+      <a href="#" class="image image--action">
         <img src="/images/thumbnails/width-240/fox-240.jpg" alt="Foxy" />
       </a>
     </div>
   </div>
   <div class="col-2">
-    <div class="thumbnail-wrapper">
-      <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+    <div class="image-wrapper">
+      <a href="#" class="image image--circle image--action">
         <img src="/images/thumbnails/width-240/cat-240.jpg" alt="Katze" />
       </a>
     </div>
   </div>
   <div class="col-2">
-    <div class="thumbnail-wrapper">
-      <a href="#" class="thumbnail thumbnail--circle thumbnail--action">
+    <div class="image-wrapper">
+      <a href="#" class="image image--circle image--action">
         <img src="/images/thumbnails/square/hedgehog-square.jpg" alt="Hedgy" />
       </a>
     </div>
   </div>
   <div class="col-2">
-    <div class="thumbnail-wrapper">
-      <a href="#" class="thumbnail thumbnail--size-large thumbnail--circle thumbnail--action">
+    <div class="image-wrapper">
+      <a href="#" class="image image--size-large image--circle image--action">
         <img src="/images/thumbnails/square/dog-square.jpg" alt="Doggo" />
       </a>
     </div>
