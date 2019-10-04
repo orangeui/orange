b9c01a9dd32e2bfbebef94961d2fa903cb227e2f
matevzgolavsek <matevz.golavsek@gmail.com>
2019-10-04 10:15:50 +0200

    Add text-decoration utilities with hover

--- site/pages/docs/typography/index.vue
+++ site/pages/docs/typography/index.vue
@@ -40,6 +40,16 @@
       <docs-simple-list :items="fontWeightsItalic" slot="body"></docs-simple-list>
     </docs-item>
 
+    <!-- Text decoration  -->
+    <docs-item title="Text decoration" :code="textAlignmentsHTML">
+      <div slot="body">
+        <p><o-label text="HTML" size="small" /> apply text decoration by adding <code>.text-{decoration}</code> class to text elements</p>
+        <p><o-label text="HTML" size="small" /> to control text decoration on hover apply text decoration by adding <code>.hover:text-{decoration}</code> class to text elements</p>
+      </div>
+
+      <docs-simple-list :items="textAlignments" slot="body"></docs-simple-list>
+    </docs-item>
+
   </div>
 </template>
 
@@ -162,7 +172,53 @@ export default {
           class: 'fs-gamma font-normal text-italic',
           code: '<p class="font-normal text-italic">400 Normal Italic</p>'
         }
-      ]
+      ],
+
+      textAlignmentsHTML: `<p class="text-underline">Text decoration underline</p>
+<p class="text-line-through">Text decoration line-through</p>
+<p class="text-no-underline">Text decoration none</p>
+
+<p class="hover:text-underline">Text decoration underline on hover</p>
+<p class="hover:text-line-through">Text decoration line-through on hover</p>
+<p class="text-underline hover:text-no-underline">Text decoration none on hover</p>`,
+      textAlignments: [
+        {
+          title: '',
+          content: 'Text decoration underline',
+          class: 'fs-gamma text-underline',
+          code: '<p class="text-underline">Text decoration underline</p>'
+        },
+        {
+          title: '',
+          content: 'Text decoration line-through',
+          class: 'fs-gamma text-line-through',
+          code: '<p class="text-line-through">Text decoration line-through</p>'
+        },
+        {
+          title: '',
+          content: 'Text decoration none',
+          class: 'fs-gamma text-no-underline',
+          code: '<p class="text-no-underline">Text decoration none</p>'
+        },
+        {
+          title: '',
+          content: 'Text decoration underline on hover',
+          class: 'fs-gamma hover:text-underline',
+          code: '<p class="hover:text-underline">Text decoration underline on hover</p>'
+        },
+        {
+          title: '',
+          content: 'Text decoration line-through on hover',
+          class: 'fs-gamma hover:text-line-through',
+          code: '<p class="hover:text-line-through">Text decoration line-through on hover</p>'
+        },
+        {
+          title: '',
+          content: 'Text decoration none on hover',
+          class: 'fs-gamma text-underline hover:text-no-underline',
+          code: '<p class="text-underline hover:text-no-underline">Text decoration none on hover</p>'
+        },
+      ],
     }
   }
 }
