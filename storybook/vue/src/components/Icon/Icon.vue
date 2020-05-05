d18266d00e2c69a39e5a3338b68d10ea8fce10b9
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-05 13:00:05 +0200

    Add icon component

---
+++ storybook/vue/src/components/Icon/Icon.vue
@@ -0,0 +1,36 @@
+<template>
+  <span :class="classes" />
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+
+  props: {
+    /**
+     * Symbol name
+     */
+    symbol: {
+      type: String,
+      required: true
+    },
+
+    /**
+     * Possible values: See [Colors page](link-to-page)
+     */
+    color: {
+      type: String,
+      required: false
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        [`icon-${this.symbol}`]: this.symbol,
+        [`color-${this.color}`]: this.color,
+      }
+    }
+  }
+}
+</script>
