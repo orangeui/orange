aee3547f9be1e97b8d8136162370b7d6b174dfca
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-12 12:09:49 +0200

    Add breadcrumbs component and stories

---
+++ storybook/vue/src/components/Breadcrumbs/Breadcrumbs.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="breadcrumbs" :class="nav_classes">
+    <router-link v-if="back_route" :to="{ name: back_route }" class="breadcrumbs__button">
+      {{ back_route_text }}
+    </router-link>
+
+    <ul>
+      <slot />
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Breadcrumbs',
+
+  props: {
+    /**
+     * Add back button - pass in <code>name</code> to route
+     */
+    // Support for Object route will be added in the future if we'll need to pass params in
+    back_route: {
+      type: [ String, Object ],
+      required: false,
+      default: null
+    },
+
+    /**
+     * Back button text
+     */
+    back_route_text: {
+      type: String,
+      required: false,
+      default: 'Back'
+    },
+
+    /**
+     * Size
+     *
+     * Possible values: <code>full</code>
+     */
+    size: {
+      type: String,
+      default: null
+    },
+
+    /**
+     * Breadcrumbs color
+     *
+     * Possible values: See [Colors page](link-to-page)
+     */
+    color: {
+      type: String,
+      default: null
+    }
+  },
+
+  computed: {
+    nav_classes () {
+      return {
+        [`breadcrumbs--${this.size}`]: this.size,
+        [`breadcrumbs--color-${this.color}`]: this.color
+      }
+    }
+  }
+}
+</script>
