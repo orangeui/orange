a96804686cc1045f6ec3308051b7e2d7a6a506c3
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-14 10:34:56 +0200

    Add EmptyState component and stories

---
+++ storybook/vue/src/components/EmptyState/EmptyState.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="empty-state" :class="classes">
+    <div class="empty-state__content">
+      <div class="empty-state__title">
+        <!-- @slot Heading slot for title -->
+        <slot name="heading"></slot>
+      </div>
+
+      <div class="empty-state__description">
+        <!-- @slot Default slot for description text -->
+        <slot></slot>
+      </div>
+
+      <div class="empty-state__buttons">
+        <!-- @slot Buttons slot for buttons -->
+        <slot name="buttons"></slot>
+      </div>
+    </div>
+
+    <div class="empty-state__image">
+      <img :src="`${image_path}`">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyState',
+
+  props: {
+    /**
+     * Path to image
+     */
+    image_path: {
+      type: String,
+      required: true,
+      default: null
+    },
+
+    /**
+     * Remove side margins
+     */
+    no_gutter: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  computed: {
+    classes () {
+      return {
+        'empty-state--no-gutter': this.no_gutter
+      }
+    }
+  }
+}
+</script>
