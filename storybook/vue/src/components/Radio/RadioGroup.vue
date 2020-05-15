0484420782bb0088174af1a4fa8c247ee09a0da2
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-15 12:47:50 +0200

    Add Radio component and stories

---
+++ storybook/vue/src/components/Radio/RadioGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form__item">
+    <div class="radio-group">
+      <div class="radio-group__label">{{ label }}</div>
+      
+      <!-- @slot Default slot to pass in <o-radio> components -->
+      <slot />
+    </div>
+
+    <div v-if="description" class="form__description">{{ description}}</div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'RadioGroup',
+
+    props: {
+      /**
+      * Group label above checkboxes
+      */
+      label: {
+        type: String,
+        required: true,
+        default: null
+      },
+
+      /**
+      * Description text below whole group
+      */
+      description: {
+        type: String,
+        required: false,
+        default: null
+      }
+    },
+
+    data () {
+      return {}
+    },
+  }
+</script>
