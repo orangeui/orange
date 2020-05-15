55260a8e6741a1de26c36e1824672f50573b4171
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-15 11:46:51 +0200

    Add Checkbox component and stories

---
+++ storybook/vue/src/components/Checkbox/CheckboxGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="form__item">
+    <div class="checkbox-group">
+      <div class="checkbox-group__label">{{ label }}</div>
+      
+      <!-- @slot Default slot to pass in <o-checkbox> components -->
+      <slot />
+    </div>
+
+    <div v-if="description" class="form__description">{{ description}}</div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'CheckboxGroup',
+
+    props: {
+      /**
+      * Group label above checkboxes
+      */
+      label: {
+        type: String,
+        required: true,
+        default: null
+      },
+      
+      /**
+      * Description text below whole group
+      */
+      description: {
+        type: String,
+        required: false,
+        default: null
+      }
+    },
+
+    data () {
+      return {}
+    },
+  }
+</script>
