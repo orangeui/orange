e299ea91fdd229111df83c6de203b93c46f2fe71
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-21 13:25:03 +0200

    Add Table component and stories

---
+++ storybook/vue/src/components/Table/TableWrapper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="table-wrapper" :class="classes" :style="{ height: height + 'px' }">
+    <!-- @slot Default slot for table component -->
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TableWrapper',
+
+  props: {
+    /**
+     * Set height 
+     */
+    height: {
+      type: [String, Number],
+      required: false,
+      default: null
+    },
+
+    /**
+     * Sticky header when scrolling
+     *
+     * Not supported in IE
+     */
+    fixed_header: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
+
+  data () {
+    return {}
+  },
+
+  computed: {
+    classes() {
+      return {
+        [`table-wrapper--fixed-header`]: this.fixed_header
+      }
+    }
+  }
+}
+</script>
