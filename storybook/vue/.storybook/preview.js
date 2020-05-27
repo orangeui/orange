f3f4468c85dfe8855c637fbbd1a329de7006c0aa
matevzgolavsek <matevz.golavsek@gmail.com>
2020-05-27 12:09:52 +0200

    Sort stories by prepanding number

--- storybook/vue/.storybook/preview.js
+++ storybook/vue/.storybook/preview.js
@@ -31,9 +31,13 @@ const paddedList = () => {
 
 addParameters({
   docs: { inlineStories: true },
+  showRoots: true,
   iframeHeight: '60px',
   options: {
     theme: orangeTheme,
+    storySort: (a, b) => {
+      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, { numeric: true })
+    },
   },
 });
 
