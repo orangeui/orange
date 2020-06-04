57d4e5f4df31f5b28c1cc3145d260517054fd169
matevzgolavsek <matevz.golavsek@gmail.com>
2020-06-04 11:26:43 +0200

    Move directives out of modules folder

--- storybook/vue/src/components/Textarea/Textarea.vue
+++ storybook/vue/src/components/Textarea/Textarea.vue
@@ -20,7 +20,7 @@
 </template>
 
 <script>
-import AutoExpand from "../../modules/directives/AutoExpand";
+import AutoExpand from "../../directives/AutoExpand";
 
 export default {
   name: "formTextarea",
