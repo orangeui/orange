a5c696c38c0e18a187d1487cfe6ea3ce5f18cb97
matevzgolavsek <matevz.golavsek@gmail.com>
2020-07-29 11:44:09 +0200

    Add search ability to dropdown and add it to form example

--- src/js/src/dropdown.js
+++ src/js/src/dropdown.js
@@ -15,11 +15,13 @@ const Dropdown = (function() {
 
   const Classes = {
     _value_class: 'has-value',
-    _open_class: 'is-open'
+    _open_class: 'is-open',
+    _search_class: 'o-form-dropdown--search'
   }
 
   const Selectors = {
     dropdown: '.o-form-dropdown',
+    dropdown_search: 'o-form-dropdown--search',
     _input: '.o-form-dropdown__input',
     _option: '.o-form-dropdown__menu li',
     _text: '.o-form-dropdown__text'
@@ -58,6 +60,10 @@ const Dropdown = (function() {
     current_dropdown.classList.remove(Classes._open_class);
   }
 
+  // Function to check if dropdown is search
+  function _isSearch (dropdown_el) {
+    return dropdown_el.classList.contains(Classes._search_class);
+  }
 
   /**
   * Public
@@ -75,17 +81,34 @@ const Dropdown = (function() {
     for (let i = 0; i < dropdowns.length; i++) {
       let dropdown = dropdowns[i]
       let current_value = dropdown.querySelector(Selectors._input).value;
+
       if (typeof current_value === 'string' && current_value !== ''){
         dropdown.querySelector(Selectors._text).innerText = current_value;
         dropdown.classList.add(Classes._value_class);
       }
 
-      // dropdown.addEventListener('click', toggleDropdown, false);
-
+      // List items
       let options = dropdown.querySelectorAll(Selectors._option);
+
       for (let ii = 0; ii < options.length; ii++) {
         let dropdown_option = options[ii];
-        dropdown_option.addEventListener('click', _selectDropdownOption, false);
+        let dropdown_option_text = dropdown_option.textContent || dropdown_option.innerText;
+
+        // Add event listener to search trough the list
+        if(_isSearch(dropdown)) {
+          dropdown.addEventListener('keyup', function () {
+            let repeat_current_value = dropdown.querySelector(Selectors._input).value;
+            let filter_value =  repeat_current_value.toUpperCase();
+
+            if (dropdown_option_text.toUpperCase().indexOf(filter_value) > -1) {
+              dropdown_option.style.display = "";
+            } else {
+              dropdown_option.style.display = "none";
+            }
+          })
+        }
+
+        dropdown_option.addEventListener('click', _selectDropdownOption, { once: true });
       }
     }
 
@@ -94,7 +117,7 @@ const Dropdown = (function() {
         event.stopPropagation();
         closeDropdowns();
       }
-    });
+    }, { once:true } );
   }
 
   // Function to toggle dropdown active class
@@ -142,9 +165,7 @@ const Dropdown = (function() {
 Dropdown.getDropdowns();
 
 // On init run activateToggles if they exist
-if (Dropdown.dropdowns) {
-  Dropdown.activateDropdowns()
-}
+Dropdown.activateDropdowns()
 
 // Add click event
 document.addEventListener('click', function (event) {
@@ -153,7 +174,8 @@ document.addEventListener('click', function (event) {
   }
 
   const isDropdownLabel = event.target.classList.contains('o-form-dropdown__label') ||
-                          event.target.classList.contains('o-form-dropdown__text')
+                          event.target.classList.contains('o-form-dropdown__text') ||
+                          event.target.classList.contains('o-form-dropdown__input')
 
   if (isDropdownLabel) {
     Dropdown.closeDropdowns()
